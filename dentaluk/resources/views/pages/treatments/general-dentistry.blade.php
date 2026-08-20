d49e85d044229f021126a13e33bca26fa39be178
Aviral Shukla <aviralshukla2612@gmail.com>
2026-08-20 12:16:02 +0530

    Add null safety checks to treatment loops to prevent crashes when categories are missing

--- dentaluk/resources/views/pages/treatments/general-dentistry.blade.php
+++ dentaluk/resources/views/pages/treatments/general-dentistry.blade.php
@@ -33,6 +33,7 @@
     <section class="section-padding" id="details">
         <div class="container custom-container">
             <div class="row g-4 justify-content-center mb-5">
+                @if(isset($category) && $category->treatments)
                 @foreach($category->treatments->where('is_published', true) as $treatment)
                 <div class="col-md-6 col-lg-3" id="treatment-{{ $treatment->id }}">
                     <div class="why-choose-card text-center p-4">
@@ -42,6 +43,7 @@
                     </div>
                 </div>
                 @endforeach
+                @endif
             </div>
         </div>
     </section>
